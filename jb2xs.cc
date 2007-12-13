14c6482adeebc99476ed06e9125ddded08d31b69
Jakub Wilk <jwilk@jwilk.net>
2007-12-13 15:46:17 +0000

    Initial release.

---
+++ jb2xs.cc
@@ -0,0 +1,46 @@
+#include <JB2Image.h>
+#include <GURL.h>
+#include <ByteStream.h>
+#include <GException.h>
+#include <GBitmap.h>
+
+#include <clocale>
+#include <sstream>
+#include <iomanip>
+
+int main(int argc, char * const argv[])
+{
+  setlocale(LC_ALL,"");
+  djvu_programname(argv[0]);
+  GArray<GUTF8String> dargv(0, argc - 1);
+  for(int i=0;i<argc;++i)
+    dargv[i] = GNativeString(argv[i]);
+  G_TRY
+  {
+    if (argc != 2)
+      return 2;
+    const GURL::Filename::UTF8 url(dargv[1]);
+    GP<ByteStream> ibs = ByteStream::create(url, "rb");
+    GP<JB2Image> jimg = JB2Image::create();
+    jimg->decode(ibs);
+    int shape_count = jimg->get_shape_count();
+    for (int i = 0; i < shape_count; i++)
+    {
+      std::ostringstream sstream;
+      sstream << std::setw(6) << std::setfill('0') << i << ".pbm";
+      const GURL::Filename::UTF8 url(sstream.str().c_str());
+      GP<ByteStream> obs = ByteStream::create(url, "wb");
+      JB2Shape &shape = jimg->get_shape(i);
+      shape.bits->save_pbm(*obs);
+    }
+  }
+  G_CATCH(ex)
+  {
+    ex.perror();
+    return 1;
+  }
+  G_ENDCATCH;
+  return 0;
+}
+
+// vim:ts=2 sw=2 et
