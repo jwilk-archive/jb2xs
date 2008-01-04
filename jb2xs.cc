ef726bd37c65aa2480c05b7a3b99aad56df0bd03
Jakub Wilk <jwilk@jwilk.net>
2008-01-04 11:53:23 +0000

    Display blit count information.

--- jb2xs.cc
+++ jb2xs.cc
@@ -33,6 +33,13 @@ int main(int argc, char * const argv[])
       JB2Shape &shape = jimg->get_shape(i);
       shape.bits->save_pbm(*obs);
     }
+    int blit_count = jimg->get_blit_count();
+    printf(">> %d\n", blit_count);
+    for (int i = 0; i < blit_count; i++)
+    {
+      const JB2Blit *blit = jimg->get_blit(i);
+      printf("%d\n", blit->shapeno);
+    }
   }
   G_CATCH(ex)
   {
